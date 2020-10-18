115415b4ed7dcaeeb102f938da31a5bb698e8980
Tyrone <developertyrone@gmail.com>
2020-10-18 21:44:11 +0800

    Day1

---
+++ HackerRank/30dayChallenge/src/com/hackerrank/Day2.java
@@ -0,0 +1,28 @@
+package com.hackerrank;
+
+import java.util.Scanner;
+
+public class Day2 {
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        System.out.println(Math.round(meal_cost * (100+tip_percent+tax_percent)/100) );
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
