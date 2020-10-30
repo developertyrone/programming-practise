2f8ab9139e67350c6cc56483618f232f950041e5
Tyrone <developertyrone@gmail.com>
2020-10-30 12:10:25 +0800

    Day14

--- HackerRank/30dayChallenge/src/com/hackerrank/Day11.java
+++ HackerRank/30dayChallenge/src/com/hackerrank/Day11.java
@@ -3,62 +3,24 @@ package com.hackerrank;
 import java.util.Scanner;
 
 
-abstract class Book {
-    String title;
-    String author;
-
-    Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-    }
-
-    abstract void display();
-}
-
-// Declare your class here. Do not use the 'public' access modifier.
-// Declare the price instance variable
-class MyBook extends Book {
-    int price;
-    MyBook(String title, String author, int price) {
-        super(title, author);
-        this.price = price;
-    }
-
-    public void display(){
-        System.out.println("Title: " + this.title);
-        System.out.println("Author: " + this.author);
-        System.out.println("Price: " + this.price);
-    }
-}
-/**
- *   Class Constructor
- *
- *   @param title The book's title.
- *   @param author The book's author.
- *   @param price The book's price.
- **/
-// Write your constructor here
-
-/**
- *   Method Name: display
- *
- *   Print the title, author, and price in the specified format.
- **/
-// Write your method here
-
-// End class
-
 public class Day11 {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String title = scanner.nextLine();
-        String author = scanner.nextLine();
-        int price = scanner.nextInt();
-        scanner.close();
+    private static final Scanner scanner = new Scanner(System.in);
 
-        Book book = new MyBook(title, author, price);
-        book.display();
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        scanner.close();
     }
 
 }
