d525d5dfb46c7e9c8d89674a723fd615c4d62bd1
Tyrone <developertyrone@gmail.com>
2020-11-01 21:31:25 +0800

    Day16

---
+++ HackerRank/30dayChallenge/src/com/hackerrank/Day16.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day16 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+
+        try{
+            int result = Integer.parseInt(S);
+            System.out.println(result);
+        }catch(NumberFormatException ex){
+            System.out.println("Bad String");
+        }
+        
+    }
+}
